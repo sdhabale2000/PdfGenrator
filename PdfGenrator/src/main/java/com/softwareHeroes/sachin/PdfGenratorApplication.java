292fc92a3ad78fa7ca03a7c161252eec82cfdb5f
sachin dhabale <sdhabale007@gmail.com>
2022-11-08 12:34:17 +0530

    new project created

---
+++ PdfGenrator/src/main/java/com/softwareHeroes/sachin/PdfGenratorApplication.java
@@ -0,0 +1,13 @@
+package com.softwareHeroes.sachin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfGenratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfGenratorApplication.class, args);
+	}
+
+}
---
+++ PdfGenrator/src/test/java/com/softwareHeroes/sachin/PdfGenratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareHeroes.sachin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfGenratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
